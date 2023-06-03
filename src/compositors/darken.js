fe84ffcc08f7761fad0201b986e1a37bdb6ec084
Abbey Sparrow <abbey@khrome.net>
2023-06-03 09:40:14 -0600

    compositors

---
+++ src/compositors/darken.js
@@ -0,0 +1,24 @@
+export const darken = (aPixels, bPixels, newPixels)=>{
+    var sx = aPixels.width; //getx
+    var sy = aPixels.height; //gety
+    for(var y = 0; y < sy; y++){
+        for(x = 0; x < sx; x++){
+            newPixels.data[((y*(sx*4)) + (x*4))     ] = Math.min(
+                aPixels.data[((y*(sx*4)) + (x*4))     ],
+                bPixels.data[((y*(sx*4)) + (x*4))     ]
+            );
+            newPixels.data[((y*(sx*4)) + (x*4)) + 1 ] = Math.min(
+                aPixels.data[((y*(sx*4)) + (x*4)) + 1 ],
+                bPixels.data[((y*(sx*4)) + (x*4)) + 1 ]
+            );
+            newPixels.data[((y*(sx*4)) + (x*4)) + 2 ] = Math.min(
+                aPixels.data[((y*(sx*4)) + (x*4)) + 2 ],
+                bPixels.data[((y*(sx*4)) + (x*4)) + 2 ]
+            );
+            newPixels.data[((y*(sx*4)) + (x*4)) + 3 ] = Math.min(
+                aPixels.data[((y*(sx*4)) + (x*4)) + 3 ],
+                bPixels.data[((y*(sx*4)) + (x*4)) + 3 ]
+            );
+        }
+    }
+}
